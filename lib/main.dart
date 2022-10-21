3c87f1db6bf1c7ba88471a39f4f4d1774357062d
kjayamanna <kaveen.jayamanna@gmail.com>
2022-10-20 23:02:30 -0500

    Google stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  runApp( //takes the given widget and makes it the root of tge widget.
+    const Center( // Widget 1 (const acts as a constructor because the class doesn't have any members.)
+      child: Text( // widget 2
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
 }
